4f974af7d4da53564ffffff6ceb543ffdd57260d
Eddison Guillermo <stevenguillnavides@gmail.com>
2026-07-20 18:39:02 -0500

    Configurar CORS para GitHub Pages

--- server.js
+++ server.js
@@ -4,7 +4,27 @@ require('dotenv').config();
 
 
 const express = require('express');
-const cors = require('cors');
+
+const cors = require("cors");
+
+const cors = require("cors");
+
+app.use(cors({
+
+    origin: [
+
+        "http://localhost:5500",
+
+        "http://127.0.0.1:5500",
+
+        "https://tiwi6x9.github.io"
+
+    ],
+
+    methods: ["POST"]
+
+}));
+
 
 const contactoRoutes = require("./routes/contacto");
 
@@ -43,9 +63,16 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log("====================================");
-    console.log("🚀 API SPE iniciada correctamente");
-    console.log(`🌐 http://localhost:${PORT}`);
-    console.log("====================================");
-});
+
+
+if (process.env.NODE_ENV !== "production") {
+
+    app.listen(PORT, () => {
+
+        console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+
+    });
+
+}
+
+module.exports = app;
