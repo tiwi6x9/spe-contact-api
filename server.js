80b425825b79a0b35544afbb009b36cf800174ce
Eddison Guillermo <stevenguillnavides@gmail.com>
2026-07-20 19:44:02 -0500

    Configurar de CORS

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-
-
-
-const express = require('express');
-
+const express = require("express");
 const cors = require("cors");
 
-
 const contactoRoutes = require("./routes/contacto");
-
 const limiter = require("./middleware/rateLimiter");
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 const PORT = process.env.PORT || 3000;
 
-
-
-
-// Middleware
 const corsOptions = {
     origin: [
         "http://localhost:5500",
@@ -31,44 +23,26 @@ const corsOptions = {
     credentials: false
 };
 
-
-app.set("trust proxy", 1);
-
-app.use(cors(corsOptions))
-
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
-//app.use(limiter);
+// Descomentar después de verificar que todo funciona
+// app.use(limiter);
 
 app.use("/api/contacto", contactoRoutes);
 
-
-
-// Ruta de prueba
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({
-        mensaje: 'API funcionando correctamente'
+        mensaje: "API funcionando correctamente"
     });
 });
 
-
-
 if (process.env.NODE_ENV !== "production") {
-
     app.listen(PORT, () => {
-
         console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
-
     });
-
 }
 
 module.exports = app;
-
-
-
-
-
-
