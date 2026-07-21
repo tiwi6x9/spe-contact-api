20628a5df80c2d04888843defdd7792e9a9c0d7f
Eddison Guillermo <stevenguillnavides@gmail.com>
2026-07-20 19:28:11 -0500

    Configurar de CORS

--- server.js
+++ server.js
@@ -20,27 +20,19 @@ const PORT = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors({
-
+const corsOptions = {
     origin: [
-
         "http://localhost:5500",
-
         "http://127.0.0.1:5500",
-
         "https://tiwi6x9.github.io"
-
     ],
-
-    methods: ["POST"],
-
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
     credentials: false
-
-}));
-
+};
 
 
-
+app.use(cors(corsOptions))
 
 app.options("*", cors());
 
