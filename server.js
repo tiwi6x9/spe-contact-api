2dde2bcee98aaffe24e6cb219797afba32724405
Eddison Guillermo <stevenguillnavides@gmail.com>
2026-07-20 19:49:18 -0500

    Configurar temporal

--- server.js
+++ server.js
@@ -12,16 +12,9 @@ app.set("trust proxy", 1);
 
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-    origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://tiwi6x9.github.io"
-    ],
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-    credentials: false
-};
+app.use(cors());
+
+app.options("*", cors());
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
